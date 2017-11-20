c8b36c9694254f225770ade385afed1e5b3a49ca
FRANKIETANG <350558468@qq.com>
2017-11-20 15:35:53 +0800

    project-init

---
+++ src/js/mod/b.js
@@ -0,0 +1,6 @@
+let a = require('./a.js')
+
+module.exports = {
+    b: 'bbbb',
+    a: a
+}
---
+++ src/webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    entry: './src/js/app/index.js',
+    output: {
+      filename: './public/js/index.js'
+    }
+  };
