3d3d3260970d898ab7794ce8693a47d324e64fee
FRANKIETANG <350558468@qq.com>
2017-11-23 21:03:32 +0800

    event

---
+++ src/js/mod/event.js
@@ -0,0 +1,41 @@
+export class Event {
+    constructor() {
+        this.onceEvent = {}
+        this.events = {}
+    }
+
+    on(event, fn) {
+        if (this.events[event]) {
+            this.events[event].push(fn)
+        } else {
+            this.events[event] = [fn]
+        }
+    }
+
+    once(event, fn) {
+        if (this.onceEvent[event]) {
+            this.onceEvent[event].push(fn)
+        } else {
+            this.onceEvent[event] = [fn]
+        }
+    }
+
+    fire(event) {
+        if (this.events[event]) {
+            this.events[event].forEach((fn) => {
+                fn()
+            })
+        }
+        if (this.onceEvent[event]) {
+            this.onceEvent[event].forEach((fn) => {
+                fn()
+            })
+            this.onceEvent[event] = []
+        }
+    }
+
+    off(event) {
+        delete this.events[event]
+    }
+    
+}
