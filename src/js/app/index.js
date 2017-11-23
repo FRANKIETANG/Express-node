9902b05f40a8427f298b0e6c454149d2c387d3b1
FRANKIETANG <350558468@qq.com>
2017-11-23 20:21:40 +0800

    mod notes

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,6 +1,9 @@
 import { Toast } from '../mod/toast.js'
 import { Waterfall } from '../mod/waterfall.js'
 
+import '../../less/note.less'
+import '../../less/index.less'
+
 let toast = new Toast()
 let waterfall = new Waterfall()
 
