3d1ac0b8daeaaf2c521fa1f1126341dea75880e9
FRANKIETANG <350558468@qq.com>
2017-11-25 21:07:55 +0800

    test database

--- model/note.js
+++ model/note.js
@@ -7,11 +7,38 @@ const sequelize = new Sequelize(undefined, undefined, undefined, {
 
 });
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log('Connection has been established successfully.');
-    })
-    .catch(err => {
-        console.error('Unable to connect to the database:', err);
-    });
+// sequelize
+//     .authenticate()
+//     .then(() => {
+//         console.log('Connection has been established successfully.');
+//     })
+//     .catch(err => {
+//         console.error('Unable to connect to the database:', err);
+//     });
+
+const Note = sequelize.define('note', {
+    text: {
+        type: Sequelize.STRING
+    }
+});
+
+// force: true 如果表已经存在，将会丢弃表
+// Note.sync(/*{ force: true }*/).then(() => {
+//     // 表已创建
+//     // return Note.create({
+//     //     firstName: 'John',
+//     //     lastName: 'Hancock'
+//     // });
+
+//     return Note.create({ text: 'holleworld' })
+// }).then(() => {
+//     Note.findAll({ raw: true }).then(notes => {
+//         console.log(notes)
+//     })
+// })
+
+// Note.create({ text: 'holleworld' })
+
+// Note.findAll({ raw: true, where: { id: 1 } }).then(notes => {
+//     console.log(notes)
+// })
